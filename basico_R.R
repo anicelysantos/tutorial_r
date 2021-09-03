d9b1542fe72543998ee1e56f07ee16c264692e97
Anicely Santos <anicelysantos@gmail.com>
2021-09-03 12:24:04 -0300

    Create basico_R.R

---
+++ basico_R.R
@@ -0,0 +1,55 @@
+#R como calculadora
+
+#Soma
+1 + 1 
+
+#Subtração
+4 - 2
+
+#Multiplicação
+2 * 3
+
+#Divisão
+5 / 3
+
+#Potência
+4 ^ 2
+
+#resto da divisão de 5 por 3
+5 %% 3
+
+#Parte inteira da divisão por 3
+5 %/% 3
+  
+#Expressão numérica  
+2 * 2 - (4+4) / 2
+
+#Numero mágico do R
+(1 + sqrt(5)) / 2
+
+#Objeto e funções
+
+a <- 1
+a
+
+#regra para nomear objetos
+
+x <- 1
+x1 <- 2
+objeto <-  3
+meu_objeto <- 4
+meu.objeto <- 5
+
+#funçõe
+sum(1,2)
+
+#Multiplique a sua idade por meses e salve o resultado em um objeto chamado
+#idade_em_meses. Em seguida, multiplique esse objeto por 30 e salve
+#o resultado em um objeto chamado idade_em_dias.
+
+idade_em_meses <- 31 * 9
+idade_em_dias <- idade_em_meses * 30
+idade_em_dias
+idade_em_meses
+
+
